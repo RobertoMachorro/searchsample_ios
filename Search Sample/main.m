08555b5a5a12703b20915cc44d3bc60f8437a989
Roberto Machorro <roberto@unpluggedideas.com>
2017-12-13 21:39:35 -0500

    Initial Commit

---
+++ Search Sample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Search Sample
+//
+//  Created by Roberto Machorro on 12/13/17.
+//  Copyright © 2017 Unplugged Ideas, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
