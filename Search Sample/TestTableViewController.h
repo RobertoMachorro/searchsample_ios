1348c8007793bb36e61d9ad61330fe18723e5d81
Roberto Machorro <roberto@unpluggedideas.com>
2017-12-25 15:46:54 -0600

    Added TestTableViewController class files and attached to storyboard.

---
+++ Search Sample/TestTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestTableViewController.h
+//  Search Sample
+//
+//  Created by Roberto Machorro on 12/25/17.
+//  Copyright © 2017 Unplugged Ideas, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestTableViewController : UITableViewController
+
+@end
